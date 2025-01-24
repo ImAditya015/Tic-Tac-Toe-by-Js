8c79e2602be1c6da41ef03b5df891eb282b68738
ImAditya015 <adityasinghchauhan600@gmail.com>
2025-01-24 13:45:56 +0530

    further code

---
+++ first.js
@@ -0,0 +1,9 @@
+let btn= document.querySelector(".btn");
+let reset= document.querySelector("#reset");
+let newGame= document.querySelector("#new-game");
+
+let turnO = true; //true for player O and false for player X
+
+let arr1 = [["maths", "science"], ["64", "68"], ["A", "A+"]];
+console.log(arr1.len);
+
